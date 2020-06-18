14b6e37c296e40bce56823f781ced711f39e0372
sujan manandhar <sujanmanandhar@sujans-Air.lan>
2020-06-18 15:39:11 -0500

    status and dataDisplay added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {TabsetComponent} from 'ngx-bootstrap/tabs';
+import {TabsModel} from './tabs.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-exercise';
+  @ViewChild('tabset', {static: false}) tabset: TabsetComponent;
+  TabSet: TabsModel[] = [
+    {value: 'Tab1'},
+    {value: 'Tab2'},
+    {value: 'Tab3'},
+  ];
+  status = 'Status ?';
+
+  dropDownList: TabsModel[] = [
+    {value: 'Happy :)'},
+    {value: 'Sad :('},
+    {value: 'Excited :D'},
+    {value: 'Crying :\'('}
+  ];
+  ngOnInit(): void {
+
+  }
+
+  dropdownClicked(val: string): void {
+    this.status = val;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {TabsModule} from 'ngx-bootstrap/tabs';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import {FormsModule} from '@angular/forms';
+import { ContentComponent } from './components/content/content.component';
+import { HeaderComponent } from './components/header/header.component';
+import { DataBarComponent } from './components/data-bar/data-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent,
+    HeaderComponent,
+    DataBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    TabsModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
